289657ec831b73ea081460360a6117bdac7ed158
Anwesh Gangula <gangulaanwesh@gmail.com>
2023-04-11 16:20:36 +0530

    18. Input Properties - Data Binding in Angular

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
 
@@ -7,6 +7,18 @@ import { ICustomer } from '../../shared/interfaces';
   templateUrl: './customers-list.component.html',
 })
 export class CustomersListComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+
   filteredCustomers: ICustomer[] = [];
   customersOrdertotal: number;
   currencyCode: string = 'USD';
