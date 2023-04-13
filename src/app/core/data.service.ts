145417af9a4c862f0543daf2b82116ed82bbc3ee
Anwesh Gangula <gangulaanwesh@gmail.com>
2023-04-13 12:33:54 +0530

    23. Angular Services and Http - Calling the Server with HttpClient

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -10,15 +10,43 @@ import { ICustomer, IOrder } from '../shared/interfaces';
 export class DataService {
   baseUrl: string = 'assets/';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getCustomers(): Observable<ICustomer[]> {
+    return this.http
+      .get<ICustomer[]>(this.baseUrl + 'customers.json')
+      .pipe(catchError(this.handleError));
+  }
+
+  getCustomer(id: number): Observable<ICustomer | null> {
+    return this.http.get<ICustomer[]>(this.baseUrl + 'customers.json').pipe(
+      map((customers) => {
+        let customer = customers.filter((cust: ICustomer) => cust.id === id);
+        return customer && customer.length ? customer[0] : null;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  getOrders(id: number): Observable<IOrder[]> {
+    return this.http.get<IOrder[]>(this.baseUrl + 'orders.json').pipe(
+      map((orders) => {
+        let custOrders = orders.filter(
+          (order: IOrder) => order.customerId === id
+        );
+        return custOrders;
+      }),
+      catchError(this.handleError)
+    );
+  }
 
   private handleError(error: any) {
     console.error('server error: ', error);
     if (error.error instanceof Error) {
       const errMessage = error.error.message;
-      throw new Error(errMessage);
+      return throwError(() => new Error(error));
     }
 
-    throw new Error(error || 'Node.js server error');
+    return throwError(() => new Error(error || 'Node.js server error'));
   }
 }
