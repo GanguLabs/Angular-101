6b8f7186876f6210e5df2cb9e628ca18d82204d9
Anwesh Gangula <gangulaanwesh@gmail.com>
2023-04-13 12:38:54 +0530

    24. Angular Services and Http - Injecting a Service into a Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CustomersModule, SharedModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    CustomersModule,
+    SharedModule,
+    CoreModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent], // this defines the startup component
 })
